9da5251f8ef2981ba0c9570af4a3505c7a256d36
Leandro Farias <leandrorf1606@gmail.com>
2025-01-31 18:05:31 -0300

    totalPrice cart

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -9,19 +9,23 @@ export function Cart({ setIsVisible }) {
   const totalPrice = useSelector(selectCartTotalPrice);
 
   return (
-    <div className="absolute h-screen bg-white text-black w-[480px] top-0 right-0 flex flex-col px-2 py-6">
-      <button className="self-end" onClick={() => setIsVisible(false)}>
-        <IoIosClose className="text-5xl" />
-      </button>
-      <h2 className="text-3xl pl-2 font-semibold mb-6">Seu Carrinho</h2>
+    <div className="absolute bg-white text-black w-[480px] top-0 right-0 flex flex-col justify-between px-2 py-6 h-screen snap-y overflow-y-auto">
+      <div className="flex flex-col">
+        <button className="self-end" onClick={() => setIsVisible(false)}>
+          <IoIosClose className="text-5xl" />
+        </button>
+        <h2 className="text-3xl pl-2 font-semibold mb-6">Seu Carrinho</h2>
 
-      <div className="pl-2 space-y-4">
-        {products.map((product) => (
-          <CartItem product={product} key={product.id} />
-        ))}
-
-        <p className="text-3xl font-bold">Total: {totalPrice}</p>
+        <div className="relative space-y-4">
+          {products.map((product) => (
+            <div key={product.id} className="snap-start">
+              <CartItem product={product} />
+              <div className="w-full h-[1px] bg-zinc-400" />
+            </div>
+          ))}
+        </div>
       </div>
+      <p className="bottom-0 text-3xl font-bold pb-4 pl-4">Total: R$ {totalPrice}</p>
     </div>
   );
 }
